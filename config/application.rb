eb6c5828ba69258dfd2fad505b0d64d7d5152b3f
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-09-21 00:56:20 -0400

    set server time

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,9 @@ Bundler.require(*Rails.groups)
 
 module StravaPersonalAnalytics
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.beginning_of_week = :sunday
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
   end
 end
