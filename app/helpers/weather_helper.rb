f68fabbd12d5485911612d1b347c6437b099d156
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-10-02 23:53:17 -0400

    open weather resilient

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -3,50 +3,53 @@ module WeatherHelper
   IMAGE_URL = 'https://openweathermap.org/img/w/{icon}.png'
 
   def current_weather
-    {
-      temperature: current_temperature,
-      image: current_weather_image,
-      wind_speed: current_weather_wind_speed,
-      wind_deg_formated: current_weather_wind_deg_formated,
-      wind_deg: current_weather_wind_deg,
-      sunset: current_weather_sunset,
-      sunrise: current_weather_sunrise,
-    }
+    weather ||= OpenWeather::Current.city(ENV.fetch('OPEN_WEATHER_MAP_LOCATION'), API_OPTIONS)
+
+    if weather['cod'] != 200
+      @weather_error_message = weather['message']
+      return nil
+    else
+      {
+        temperature: current_temperature(weather),
+        image: current_weather_image(weather),
+        wind_speed: current_weather_wind_speed(weather),
+        wind_deg_formated: current_weather_wind_deg_formated(weather),
+        wind_deg: current_weather_wind_deg(weather),
+        sunset: current_weather_sunset(weather),
+        sunrise: current_weather_sunrise(weather),
+      }
+    end
   end
 
   private
 
-  def current_weather_sunset
-    Time.at(weather_right_now['sys']['sunset']).in_time_zone(ENV.fetch('TIMEZONE')).strftime('%I:%M%P')
+  def current_weather_sunset(weather)
+    Time.at(weather['sys']['sunset']).in_time_zone(ENV.fetch('TIMEZONE')).strftime('%I:%M%P')
   end
 
-  def current_weather_sunrise
-    Time.at(weather_right_now['sys']['sunrise']).in_time_zone(ENV.fetch('TIMEZONE')).strftime('%I:%M%P')
+  def current_weather_sunrise(weather)
+    Time.at(weather['sys']['sunrise']).in_time_zone(ENV.fetch('TIMEZONE')).strftime('%I:%M%P')
   end
 
-  def current_weather_wind_speed
-    "#{weather_right_now['wind']['speed'].round} km/h"
+  def current_weather_wind_speed(weather)
+    "#{weather['wind']['speed'].round} km/h"
   end
 
-  def current_weather_wind_deg_formated
-    return '0°' unless !!weather_right_now['wind']['deg']
-    "#{weather_right_now['wind']['deg'].round}°"
+  def current_weather_wind_deg_formated(weather)
+    return '0°' unless !!weather['wind']['deg']
+    "#{weather['wind']['deg'].round}°"
   end
 
-  def current_weather_wind_deg
-    return 0 unless !!weather_right_now['wind']['deg']
-    weather_right_now['wind']['deg'].round
+  def current_weather_wind_deg(weather)
+    return 0 unless !!weather['wind']['deg']
+    weather['wind']['deg'].round
   end
 
-  def current_temperature
-    weather_right_now["main"]["temp"].round
+  def current_temperature(weather)
+    weather["main"]["temp"].round
   end
 
-  def current_weather_image
-    IMAGE_URL.gsub('{icon}', weather_right_now['weather'][0]['icon'])
-  end
-
-  def weather_right_now
-    weather_current ||= OpenWeather::Current.city(ENV.fetch('OPEN_WEATHER_MAP_LOCATION'), API_OPTIONS)
+  def current_weather_image(weather)
+    IMAGE_URL.gsub('{icon}', weather['weather'][0]['icon'])
   end
 end
